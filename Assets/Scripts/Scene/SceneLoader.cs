a99a4b7d72bd09c8a5a7dd16b16aac24b94175ff
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-05 23:45:49 +0200

    Transition from load scene to start menu created with delay

--- Assets/Scripts/Scene/SceneLoader.cs
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    int currentSceneIndex;
+    private int loadSceneDelayTime = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // Wait till the background sound is finished(5 seconds)
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(DelayLoadScene());
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    // Delay
+    IEnumerator DelayLoadScene()
+    {
+        yield return new WaitForSeconds(loadSceneDelayTime);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
