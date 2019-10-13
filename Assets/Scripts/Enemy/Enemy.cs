8ad2f8563d6328305b54f37ed441086207b647dd
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-13 20:09:40 +0200

    Enemy movement created

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{   
+    // For readability in engine
+    [Header("Enemy Configs")]
+    [Range(0f, 10f)] [SerializeField] float movingSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemy();
+    }
+    
+    // Move enemy (independent from the FPS of device)
+    private void MoveEnemy()
+    {
+        transform.Translate(Vector2.left * movingSpeed * Time.deltaTime);
+    }
+}
