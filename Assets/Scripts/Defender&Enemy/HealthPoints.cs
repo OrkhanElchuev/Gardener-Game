5b840d5b8aa2058d18eaad430284fcabb4881c07
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-15 00:05:41 +0200

    Health points added to enemies, enemy dying added

---
+++ Assets/Scripts/Defender&Enemy/HealthPoints.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPoints : MonoBehaviour
+{
+    [SerializeField] float healthPoints = 100.0f;
+
+    public void DealDamage(float damage)
+    {
+        healthPoints -= damage;
+        if(healthPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Defender/Projectile.cs
+++ Assets/Scripts/Defender/Projectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 1.0f;
+    [SerializeField] int damageAmount = 50;
 
     // Update is called once per frame
     void Update()
@@ -16,5 +17,17 @@ public class Projectile : MonoBehaviour
     private void MoveProjectile()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        var health = otherObject.GetComponent<HealthPoints>();
+        var enemy = otherObject.GetComponent<Enemy>();
+
+        if (enemy && health)
+        {
+            health.DealDamage(damageAmount);
+            Destroy(gameObject);
+        }
     }
 }
