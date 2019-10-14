e93d052cea0b794c6ed505335f4ae5dd2d300ae5
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-14 22:35:18 +0200

    Defender created, animation tweaked, projectile created

---
+++ Assets/Scripts/Defender/Shooter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public void Shoot()
+    {
+        return;
+    }
+}
