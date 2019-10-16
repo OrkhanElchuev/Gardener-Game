f4077f20a66da43b48835e85fd035ead11d5037c
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-16 23:22:54 +0200

    Spawning defender on mouse click added, Game Play interaction area created

---
+++ Assets/Scripts/Defender/DefenderSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawn : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+
+    // On mouse click 
+    private void OnMouseDown()
+    {
+        SpawnDefender(GetFieldClicked());
+    }
+
+    // Instantiate defender object according to the mouse position
+    private void SpawnDefender(Vector2 worldPosition)
+    {
+        GameObject newDefender = Instantiate(defender,
+        worldPosition, Quaternion.identity) as GameObject;
+    }
+
+    // Get the coordinates of mouse click within Game Play field
+    private Vector2 GetFieldClicked()
+    {
+        // Get the mouse click position
+        Vector2 clickPosition = new Vector2(Input.mousePosition.x, 
+        Input.mousePosition.y);
+        // Adjust it according to the current Camera view
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(clickPosition);
+        return worldPosition;
+    }
+}
