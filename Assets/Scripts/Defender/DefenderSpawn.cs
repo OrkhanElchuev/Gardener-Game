98a5f351a5c5d28e6e354f2eb9cd022c0115dd94
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-16 23:50:39 +0200

    Play Area divided into grid, placing defenders on the relevant fields

--- Assets/Scripts/Defender/DefenderSpawn.cs
+++ Assets/Scripts/Defender/DefenderSpawn.cs
@@ -13,20 +13,28 @@ public class DefenderSpawn : MonoBehaviour
     }
 
     // Instantiate defender object according to the mouse position
-    private void SpawnDefender(Vector2 worldPosition)
+    private void SpawnDefender(Vector2 roundedWorldPos)
     {
         GameObject newDefender = Instantiate(defender,
-        worldPosition, Quaternion.identity) as GameObject;
+        roundedWorldPos, Quaternion.identity) as GameObject;
+    }
+
+    private Vector2 SnapPlayAreaToGrid(Vector2 worldPosition)
+    {
+        float modifiedX = Mathf.RoundToInt(worldPosition.x);
+        float modifiedY = Mathf.RoundToInt(worldPosition.y);
+        return new Vector2(modifiedX, modifiedY);
     }
 
     // Get the coordinates of mouse click within Game Play field
     private Vector2 GetFieldClicked()
     {
         // Get the mouse click position
-        Vector2 clickPosition = new Vector2(Input.mousePosition.x, 
+        Vector2 clickPosition = new Vector2(Input.mousePosition.x,
         Input.mousePosition.y);
         // Adjust it according to the current Camera view
         Vector2 worldPosition = Camera.main.ScreenToWorldPoint(clickPosition);
-        return worldPosition;
+        Vector2 gridPosition = SnapPlayAreaToGrid(worldPosition);
+        return gridPosition;
     }
 }
