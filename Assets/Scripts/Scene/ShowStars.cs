7a1a4331b1bfcb9058aabaf613dbfa4777e3fa79
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-17 17:35:10 +0200

    Animation is fully recreated for enemy and defender, parent and child relationship added to objects

---
+++ Assets/Scripts/Scene/ShowStars.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowStars : MonoBehaviour
+{
+    [SerializeField] int stars = 100;
+    TextMeshProUGUI starText;
+
+    void Start()
+    {
+        starText = GetComponent<TextMeshProUGUI>();
+        UpdateDisplay();
+    }
+
+    public void AddStars(int amountOfStars)
+    {
+        stars += amountOfStars;
+        UpdateDisplay();
+    }
+
+    public void SpendStars(int amountOfStars)
+    {
+        if (stars >= amountOfStars)
+        {
+            stars -= amountOfStars;
+            UpdateDisplay();
+        }
+    }
+
+    private void UpdateDisplay()
+    {
+        starText.text = stars.ToString();
+    }
+
+}
