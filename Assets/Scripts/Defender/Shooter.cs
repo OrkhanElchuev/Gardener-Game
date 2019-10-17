58d0e30c7e922d27cdc5b3ec5340518df2d7463d
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-17 21:29:50 +0200

    Defender shooting condition bug fixed, description added to functions

--- Assets/Scripts/Defender/Shooter.cs
+++ Assets/Scripts/Defender/Shooter.cs
@@ -14,13 +14,15 @@ public class Shooter : MonoBehaviour
     }
 
     private void Update()
-    {
-        if (IsAttackerInLane())
+    {   
+        if (IsEnemyInLane())
         {
+            // TODO switch to shooting
             Debug.Log("Shoot");
         }
         else
         {
+            // TODO switch to idle
             Debug.Log("Wait");
         }
     }
@@ -30,23 +32,27 @@ public class Shooter : MonoBehaviour
         Instantiate(tomatoShooting, weapon.transform.position, Quaternion.identity);
     }
 
-    private bool IsAttackerInLane()
+    // Check if there is an enemy in the same lane with current defender
+    private bool IsEnemyInLane()
     {
+        // Get the number of elements under spawning lanes
         if (myLaneSpawner.transform.childCount <= 0)
         {
             return false;
         }
-        else
-        {
-            return true;
-        }
+        return true;
     }
 
     private void SetLaneSpawner()
     {
         EnemySpawner[] spawners = FindObjectsOfType<EnemySpawner>();
+        // Iterate through an array of EnemySpawner objects
         foreach (EnemySpawner spawner in spawners)
         {
+            /*Check if y coordinate of spawner is almost identical
+             with y coordinate of defender 
+             Mathf.Epsilon is used to compare two floating numbers 
+             considering truncation */
             bool isCloseEnough = (Mathf.Abs(spawner.transform.position.y -
             transform.position.y) <= Mathf.Epsilon);
             if (isCloseEnough)
