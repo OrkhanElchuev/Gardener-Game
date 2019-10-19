45dd0630a6865659219f1a44dc874ee42f77342e
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-20 00:18:56 +0200

    Slider is polished, animation added to level progress slider

---
+++ Assets/Scripts/Scene/GameTimer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [SerializeField] float levelTimeInSec = 10;
+
+    private void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTimeInSec;
+
+        bool timerFinished = (Time.timeSinceLevelLoad >= levelTimeInSec);
+        if(timerFinished)
+        {
+            Debug.Log("level timer expired");
+        }
+    }
+}
