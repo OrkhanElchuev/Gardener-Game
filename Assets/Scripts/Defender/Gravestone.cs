a9659fa282ee18f0e16e21303a5d1540729dd2a0
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-19 23:20:08 +0200

    New enemy added, all needed functionalities are created

---
+++ Assets/Scripts/Defender/Gravestone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravestone : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D otherCollider)
+    {
+        Enemy enemy = otherCollider.GetComponent<Enemy>();
+    
+        if (enemy)
+        {
+            // run animation
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/FoxEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxEnemy : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+        // If there is a gravestone on a lane, jump over it
+        if (otherObject.GetComponent<Gravestone>())
+        {
+            // Setting jump trigger animation
+            GetComponent<Animator>().SetTrigger("jumpTrigger");
+        }
+        // Check if the colliding object is a defender, if yes then attack 
+        else if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Enemy>().Attack(otherObject);
+        }
+    }
+}
