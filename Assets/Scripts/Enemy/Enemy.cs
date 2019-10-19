afdbf86cb9f7f38e640796be88e5fcad719759c5
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-19 17:24:15 +0200

    Enemy collides with defenders

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private float currentEnemySpeed = 1.0f;
+    private GameObject currentTarget;
 
     // Update is called once per frame
     void Update()
     {
         MoveEnemy();
+    }
+
+    // Switch enemy to attacking state animation
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("isAttacking", true);
+        // Set current target of enemy
+        currentTarget = target;
     }
 
     // Move enemy (independent from the FPS of device)
---
+++ Assets/Scripts/Enemy/LizardEnemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LizardEnemy : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+        // Check if the colliding object is a defender, if yes then attack 
+        if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Enemy>().Attack(otherObject);
+        }
+    }
+}
