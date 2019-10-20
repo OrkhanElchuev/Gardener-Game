f7fe123755abf2af806cb258eebb16925d5ef8f9
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-21 00:43:37 +0200

    Testing in-game background music, tweaking volume by slider added

---
+++ Assets/Scripts/PlayerData/PlayerPrefsManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+    const string VOLUME_KEY = "sound";
+    const string DIFFICULTY_KEY = "difficulty";
+    const float MIN_SOUND = 0.0f;
+    const float MAX_SOUND = 1.0f;
+
+    public static void SetSound(float volume)
+    {
+        // Check for the correct value of volume in a range from 0 to 1
+        if (volume >= MIN_SOUND && volume <= MAX_SOUND)
+        {
+            Debug.Log("Sound set to " + volume);
+            PlayerPrefs.SetFloat(VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("Sound is out of range");
+        }
+    }
+
+    public static float GetSound()
+    {
+        return PlayerPrefs.GetFloat(VOLUME_KEY);
+    }
+
+}
---
+++ Assets/Scripts/Scene/OptionsManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsManager : MonoBehaviour
+{
+    [SerializeField] Slider soundSlider;
+    [SerializeField] float defaultSound = 0.7f;
+
+    void Start()
+    {
+        soundSlider.value = PlayerPrefsManager.GetSound();
+    }
+
+    void Update()
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer)
+        {
+            musicPlayer.SetSound(soundSlider.value);
+        }
+        else
+        {
+            Debug.LogWarning("No music player found");
+        }
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsManager.SetSound(soundSlider.value);
+        FindObjectOfType<SceneLoader>().LoadStartScene();
+    }
+}
