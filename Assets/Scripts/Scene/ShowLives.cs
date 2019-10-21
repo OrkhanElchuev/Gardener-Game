160f4ed58fa2adb0818cdfdd2c7955ab00023c47
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-21 16:43:54 +0200

    Enemy collider bug fixed, temporary difficulty config added

--- Assets/Scripts/Scene/ShowLives.cs
+++ Assets/Scripts/Scene/ShowLives.cs
@@ -6,14 +6,29 @@ using TMPro;
 
 public class ShowLives : MonoBehaviour
 {
-    [SerializeField] int lives = 100;
+    private float lives = 100f;
     TextMeshProUGUI livesText;
     private int damage = 20;
 
     void Start()
     {
+        DifficultySettings();
         livesText = GetComponent<TextMeshProUGUI>();
         UpdateDisplay();
+    }
+
+    // Temporary method, will be modified later
+    // CREATED FOR TESTING PURPOSES
+    private void DifficultySettings()
+    {
+        if (PlayerPrefsManager.GetGameDifficulty() == 1)
+        {
+            lives = 50;
+        }
+        else if (PlayerPrefsManager.GetGameDifficulty() == 2)
+        {
+            lives = 20;
+        }
     }
 
     public void DecrementLife()
@@ -21,7 +36,7 @@ public class ShowLives : MonoBehaviour
         lives -= damage;
         UpdateDisplay();
         // Load loosing scene if base is destroyed
-        if(lives <= 0)
+        if (lives <= 0)
         {
             FindObjectOfType<LevelManager>().ExecuteLoseCondition();
         }
