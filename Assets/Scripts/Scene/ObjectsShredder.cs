aa4dd926570c561adbb2a132e0977833d335ee55
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-24 00:25:03 +0200

    Game completed design changed, Defender Selector UI changed, Shredder added for destroying redundant projectiles

---
+++ Assets/Scripts/Scene/ObjectsShredder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsShredder : MonoBehaviour
+{
+    // Shredder for projectiles leaving the screen 
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        // Destroy object entering the collider
+        Destroy(otherCollider.gameObject);
+    }
+}
