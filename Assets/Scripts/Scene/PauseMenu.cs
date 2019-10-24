4c9a6f4ee091106fde0dbb8287d4f61227f99b54
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-10-24 03:20:35 +0200

    Game Pause function added

---
+++ Assets/Scripts/Scene/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    [SerializeField] GameObject pauseMenuCanvas;
+    [SerializeField] SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        pauseMenuCanvas.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        // Make pause canvas inactive
+        pauseMenuCanvas.SetActive(false);
+        // Resume the game flow
+        Time.timeScale = 1.0f;
+        GameIsPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuCanvas.SetActive(true);
+        // Freeze the game flow
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
+
